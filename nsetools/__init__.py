0a4f3a7afb9df4c584599f3f165e4e6f8862b561
tksundar <53440744+tksundar@users.noreply.github.com>
2020-11-07 17:55:14 +0530

    removed unused imports

--- nsetools/__init__.py
+++ nsetools/__init__.py
@@ -1,3 +1,8 @@
 """
-Created by Sundar on 07-11-2020.email tksrajan@gmail.com
+This entire package is a copy of vsjha/nsetools on github. I have fixed an issue where on android
+phones, SSL_CERTIFICATE verification was failing. I dont see any commits to this repo after Feb 19,
+hence getting this fix in seemed to be a problem. I have fixed this in my forked repo , but somehow
+pip was not able to install this. As a last resort I have copied the source files and fixed this locally
+without any prejudice to the original authors. I have raised a pull request for the same and once that is
+merged , I can get rid of these local copies
 """
