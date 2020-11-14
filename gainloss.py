5bc4418fd787a2453677defe065d176edc70d7e5
tksundar <53440744+tksundar@users.noreply.github.com>
2020-11-14 10:12:41 +0530

    trying performance improvement

--- gainloss.py
+++ gainloss.py
@@ -84,7 +84,7 @@ class GainLossScreen(BaseGrid):
         if plt == 'Windows':
             font_size = '[size=10]'
         else:
-            font_size = '[size=20]'
+            font_size = '[size=25]'
         for index, fragment in enumerate(c_data):
             row_data = []
             for item in fragment:
